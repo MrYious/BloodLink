584c7a3ae8849b6ba4bdee7a858dbd65a500342f
MrYious <rosariomark37@gmail.com>
2022-11-05 17:53:48 +0800

    feat: update template

--- app/src/components/Template.js
+++ app/src/components/Template.js
@@ -3,7 +3,7 @@ import { useEffect, useState } from 'react';
 const Template = () => {
 
   return (
-    <section className='flex w-full min-h-screen bg-red-50'>>
+    <section className='flex w-full min-h-screen bg-red-50'>
       Template
     </section>
   );
