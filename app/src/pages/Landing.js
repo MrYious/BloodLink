231aad7a785bfe7317f3f6ba7441536b84b5dbd1
MrYious <rosariomark37@gmail.com>
2022-11-05 21:37:05 +0800

    feat: add pages

--- app/src/pages/Landing.js
+++ app/src/pages/Landing.js
@@ -6,22 +6,22 @@ const Landing = () => {
 
   return (
     <div className='bg-red-50'>
-      <section className='flex flex-col w-full min-h-screen '>
+      <section id='home' className='flex flex-col w-full min-h-screen '>
         <NavigationBar />
         <div className='flex justify-center items-center flex-col h-[90vh] '>
           Home
         </div>
       </section>
-      <section className='flex items-center justify-center w-full min-h-screen '>
+      <section id='features' className='flex items-center justify-center w-full min-h-screen '>
         Features
       </section>
-      <section className='flex items-center justify-center w-full min-h-screen '>
+      <section id='contact' className='flex items-center justify-center w-full min-h-screen '>
         Contact
       </section>
-      <section className='flex items-center justify-center w-full min-h-screen '>
+      <section id='about' className='flex items-center justify-center w-full min-h-screen '>
         About
       </section>
-      <section className='flex items-center justify-center w-full min-h-screen'>
+      <section id='footer' className='flex items-center justify-center w-full min-h-screen'>
         Footer
       </section>
     </div>
