6a568d0effcefe7935cd365f3289de26cae7b2c2
MrYious <rosariomark37@gmail.com>
2022-11-05 15:47:14 +0800

    feat: update template

--- app/src/components/Template.js
+++ app/src/components/Template.js
@@ -2,22 +2,11 @@ import {useEffect, useState} from 'react';
 
 const Template = () => {
 
-    const [val, setVal] = useState('Val')
-    console.log('Template 1', val)
-
-    useEffect(() => {
-      console.log('Template 2', val)
-
-      return () => {
-        console.log('Template 3', val)
-      }
-    }, [])
-
-    return (
-        <div className="">
-          Sample Component
-        </div>
-      );
+  return (
+    <div className="">
+      Sample Component
+    </div>
+  );
 }
 
 export default Template;
