c68cf207869830e3073abadef441a7dc2ac4954f
MrYious <rosariomark37@gmail.com>
2022-11-06 15:27:59 +0800

    feat: add image

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'hero-banner': "url('./assets/images/bgHeroBanner.jpg')",
+      }
+    },
   },
   plugins: [],
 }
