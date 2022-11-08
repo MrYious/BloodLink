28f868e0ee097cd6604754b53032f80b786fed00
MrYious <rosariomark37@gmail.com>
2022-11-08 16:16:40 +0800

    feat: completed landing page

--- app/src/pages/Landing.js
+++ app/src/pages/Landing.js
@@ -70,20 +70,18 @@ const Landing = () => {
       </section>
       {/* CONTACT */}
       <section id='contact' className='flex flex-col items-center justify-center w-full min-h-[90vh] bg-cover bg-contact bg-center py-16'>
-        <div className="flex flex-col items-center justify-center rounded overflow-hidden w-[85%] md:[80%] gap-10 p-10 bg-blue-50 shadow-md shadow-black">
-
-          <div className="flex flex-col gap-1 bg-gray-100">
+        <div className="flex flex-col items-center justify-center rounded overflow-hidden w-[85%] md:[80%] gap-10 p-10 bg-slate-300 shadow-md shadow-black">
+          <div className="flex flex-col gap-1 ">
             <div className="px-5 text-4xl">
               Contact Us
             </div>
             <div className="w-full h-0 border-[3px] border-red-700 rounded-full"></div>
           </div>
-
           <div className="flex flex-col items-start justify-center w-full gap-10 lg:flex-row">
             <form className="flex flex-col items-center justify-start w-[100%] lg:w-[40%]  gap-4 bg-opacity-80 backdrop-blur-sm">
-              <input type="text" placeholder="Name" required className="w-full p-2 border-[1px] border-gray-900 rounded focus:text-gray-700 focus:border-red-800 focus:outline-none"/>
-              <input type="text" placeholder="Email Address" required className="w-full p-2 border-[1px] border-gray-900 rounded focus:text-gray-700 focus:border-red-800 focus:outline-none"/>
-              <textarea placeholder="Message" rows={5} required className="w-full p-2 border-[1px] border-gray-900 rounded resize-none focus:text-gray-700 focus:border-red-800 focus:outline-none"/>
+              <input type="text" placeholder="Name" required className="w-full p-2 border-[1px] border-gray-900 rounded bg-slate-200 focus:text-gray-700 focus:border-red-800 focus:outline-none"/>
+              <input type="text" placeholder="Email Address" required className="w-full p-2 border-[1px] border-gray-900 rounded bg-slate-200 focus:text-gray-700 focus:border-red-800 focus:outline-none"/>
+              <textarea placeholder="Message" rows={5} required className="w-full p-2 border-[1px] border-gray-900 rounded resize-none bg-slate-200 focus:text-gray-700 focus:border-red-800 focus:outline-none"/>
               <button className="w-full p-2 font-bold text-white bg-red-800 rounded shadow-sm resize-none hover:bg-red-900 shadow-black ">SEND</button>
             </form>
             <div className="flex flex-col w-[100%] lg:w-[60%] gap-10 ">
@@ -156,11 +154,51 @@ const Landing = () => {
         </div>
       </section>
       {/* ABOUT */}
-      <section id='about' className='flex items-center justify-center w-full min-h-[90vh] '>
-        About
+      <section id='about' className='flex flex-col items-center justify-center w-full min-h-[90vh] bg-gray-200 '>
+        <div className="flex flex-col gap-1 py-10">
+          <div className="px-5 text-4xl">
+            About Us
+          </div>
+          <div className="w-full h-0 border-[3px] border-red-700 rounded-full"></div>
+        </div>
+        {/* ROW 1 */}
+        <div className="flex items-center justify-around gap-10 min-h-[40vh]  w-full py-5 flex-col lg:flex-row">
+          <div className="flex flex-col gap-4 w-[85%] lg:w-[45%]   p-3">
+            <div className="flex flex-col text-5xl lg:text-6xl">
+              <div className="">What is  </div>
+              <div className="font-bold text-red-800">BloodLink ?</div>
+            </div>
+            <div className="text-justify ">
+              BloodLink is a community of people motivated to help save others thru blood donation. In this platform, people are motivated to become blood donors where any people can browse, search and ask for help.
+            </div>
+          </div>
+          <div className="w-[85%] lg:w-[35%]  bg-about3 bg-cover h-80 rounded-md shadow-md shadow-black ">
+          </div>
+        </div>
+        {/* ROW 2 */}
+        <div className="flex items-center justify-evenly min-h-[30vh] w-full py-5 flex-col-reverse lg:flex-row gap-10">
+          <div className="w-[85%] lg:w-[35%]  bg-about1 bg-cover h-80 rounded-md shadow-md shadow-black ">
+          </div>
+          <div className="flex flex-col gap-4 w-[85%] lg:w-[22%]  p-3">
+            <div className="text-2xl font-bold">
+              Our <span className=" border-b-[1px] border-red-800">Mission</span>
+            </div>
+            <div className="text-justify ">
+              To create and maintain a healthy community of donors by providing an easy-to-use social platform where they can connect.
+            </div>
+          </div>
+          <div className="flex flex-col gap-4 w-[85%] lg:w-[22%]  p-3">
+            <div className="text-2xl font-bold">
+              Our <span className=" border-b-[1px] border-red-800">Vision</span>
+            </div>
+            <div className="text-justify ">
+              To be one of pioneers of a united health system where people are continuously being motivated and inspired to work together.
+            </div>
+          </div>
+        </div>
       </section>
       {/* FOOTER */}
-      <section id='footer' className='flex w-full h-[30vh] bg-slate-300'>
+      <section id='footer' className='hidden lg:flex w-full h-[30vh] bg-slate-300'>
         {/* Column 1 */}
         <div className="flex flex-col items-center justify-center w-[35%] gap-2 py-5 ">
           <Link to={'/'} className='text-[55px] p-4 flex items-center shrink-0 leading-tight'>
