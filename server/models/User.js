39a4d46a561e3fd370fd18e8c80109e0b870cf8e
MrYious <rosariomark37@gmail.com>
2022-11-12 12:00:08 +0800

    feat: added sample model

---
+++ server/models/User.js
@@ -0,0 +1,47 @@
+import { Sequelize } from "sequelize";
+import dbConfig from "../config/dbConfig";
+
+const { DataTypes } = Sequelize;
+
+const User = db.define('user',{
+    addressID:{
+        type: DataTypes.INTEGER
+    },
+    lastname:{
+        type: DataTypes.STRING
+    },
+    firstname:{
+        type: DataTypes.STRING
+    },
+    middlename:{
+        type: DataTypes.STRING
+    },
+    gender:{
+        type: DataTypes.STRING
+    },
+    age:{
+        type: DataTypes.INTEGER
+    },
+    mobileNo:{
+        type: DataTypes.STRING
+    },
+    email:{
+        type: DataTypes.STRING
+    },
+    profilePicture:{
+        type: DataTypes.STRING,
+    },
+    bloodType:{
+        type: DataTypes.STRING
+    },
+    password:{
+        type: DataTypes.STRING
+    },
+    accountType:{
+        type: DataTypes.STRING
+    },
+},{
+    freezeTableName: true
+});
+
+export default User;
