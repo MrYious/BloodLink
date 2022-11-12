d6539574dd5fa9137bfa0d9a661507fd1e5bbca2
MrYious <rosariomark37@gmail.com>
2022-11-12 19:49:00 +0800

    feat: fix base link

--- app/src/App.js
+++ app/src/App.js
@@ -10,7 +10,7 @@ import { createContext } from 'react';
 
 export const MainContext = createContext();
 
-const link = window.location.href !== 'http://localhost:3000/' ? 'http://localhost:3001/' : 'https://blood-link.herokuapp.com/';
+const link = window.location.href.includes('http://localhost:3000/') ? 'http://localhost:3001/' : 'https://bloodlink.netlify.app/';
 
 const contextData = {
   link: link
