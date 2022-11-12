2620f71844cb81f5e584b935a309778c9839c786
MrYious <rosariomark37@gmail.com>
2022-11-12 11:45:15 +0800

    feat: added routes

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+import apiRoutes from "./routes/apiRoutes.js";
 import cors from "cors";
 import dbConfig from "./config/dbConfig.js";
 import express from "express";
@@ -15,17 +16,16 @@ const app = express();
 
 try {
   await dbConfig.authenticate();
-  console.log('Database connected...');
+  console.log('Database Connected...');
 } catch (error) {
-  console.error('Connection error:', error);
+  console.error('Database Connection Error:', error);
 }
 
 app.use(cors(corsOptions))
 app.use(express.static(path.resolve('./app/build')));
 
-app.get("/api", (req, res) => {
-  res.json({ message: "Hello from server!" });
-});
+// ROUTES -> CONTROLLERS -> MODELS
+app.use("/api", apiRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
