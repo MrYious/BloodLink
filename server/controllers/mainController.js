af0fa0067c2029ba55835aa163d5172ddc8ca1e5
MrYious <rosariomark37@gmail.com>
2022-11-12 22:45:39 +0800

    feat: complete handle register action

--- server/controllers/mainController.js
+++ server/controllers/mainController.js
@@ -4,43 +4,70 @@ import DonorInfo from "../models/DonorInfo.js";
 import DonorRequest from "../models/DonorRequest.js";
 import Review from "../models/Review.js";
 import User from "../models/User.js";
+import bcrypt from "bcrypt"
 
 // LIST OF FUNCTIONS (CRUD & VALIDATION)
 
+const saltRounds = 10;
 
 // 
 // 
 // 
 export const createNewUser = async (req, res) => {
     try {
-        // const checkUser = await User.findOne({
-        //     where: {
-        //         email: req.body.user.email
-        //     }
-        // });
-        // if(checkUser){
-        //     res.status(400).json({ message: "Email is already registered" });
-        // }
-        // console.log("BODY: ", req.body)
+        // console.log("BODY: ", req.body.data)
 
-        // const address = await Address.create(req.body.address);
-        // console.log("New Address ID ", address.id);
-        // console.log("Address Created")
+        const checkUser = await User.findOne({
+            where: {
+                email: req.body.data.personal.email
+            }
+        });
 
-        // req.body.user.password = bcrypt.hashSync(req.body.user.password, saltRounds);
-        // req.body.user.addressID = address.id;
-        // const user = await User.create(req.body.user);
-        // console.log("New User ID ", user.id);
-        // console.log("User Created");
-        // console.log("User Created",req.body.accountType);
+        if(checkUser){
+            res.status(400).json({ message: "Email is already registered!" });
+        } else if(req.body.data.security.password !== req.body.data.security.confirmPassword){
+            res.status(400).json({ message: "Passwords does not match!" });
+        } else {
 
-        // if(req.body.user.accountType === 'Donor'){
-        //     const donor = await DonorInfo.create({donorID: user.id});
-        //     console.log("New Donor ID", donor.id);
-        //     console.log("DonorInfo Created");
-        // }
+            const userData = {
+                firstname: req.body.data.personal.firstName,
+                lastname: req.body.data.personal.lastName,
+                middlename: req.body.data.personal.middleName,
+                gender: req.body.data.personal.gender,
+                age: req.body.data.personal.age,
+                mobileNumber: req.body.data.personal.mobileNo,
+                email: req.body.data.personal.email,
+                bloodGroup: req.body.data.security.bloodGroup,
+                password: bcrypt.hashSync(req.body.data.security.password, saltRounds),
+                shortBio: req.body.data.personal.bio,
+            }
+            console.log(userData);
 
-        res.status(200).json({ message: "Register Success" });
+            const user = await User.create(userData);
+
+            const addressData = {
+                userID: user.id,
+                region: req.body.data.address.region,
+                province: req.body.data.address.province,
+                city: req.body.data.address.city,
+                barangay: req.body.data.address.barangay,
+                addressLine1: req.body.data.address.line1,
+                addressLine2: req.body.data.address.line2,
+            }
+            console.log(addressData);
+
+            const address = await Address.create(addressData);
+
+            const donorInfoData = {
+                userID: user.id,
+            }
+            console.log(donorInfoData);
+
+            const donor = await DonorInfo.create(donorInfoData);
+
+            res.status(200).json({ message: "Register Success!" });
+        }
+
     } catch (error) {
         res.status(400).json({ message: error.message });
     }
