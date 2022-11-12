7025f26b949a8cb0df7262e10b8aa892df2074c1
MrYious <rosariomark37@gmail.com>
2022-11-12 11:45:35 +0800

    feat: added routes

---
+++ server/routes/apiRoutes.js
@@ -0,0 +1,25 @@
+import {
+    // createRequest,
+    // createUser,
+    // getAllDonors,
+    // getRequestsByID,
+    // seedUser,
+    // updateRequest,
+    // updateUserProfile,
+    // validateUserByEmail
+} from "../controllers/mainController.js";
+
+import express from "express";
+
+const apiRouter = express.Router();
+
+// router.post('/login', validateUserByEmail);
+// router.post('/register', createUser)
+// router.patch('/updateProfile', updateUserProfile);
+// router.get('/donors', getAllDonors);
+// router.post('/request', createRequest);
+// router.patch('/request', updateRequest);
+// router.post('/requestList', getRequestsByID);
+// router.post('/seedusers', seedUser);
+
+export default apiRouter;
