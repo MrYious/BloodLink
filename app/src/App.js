1f534d34c96c45775b2ad855b7ca602e1fd60050
MrYious <rosariomark37@gmail.com>
2022-11-17 13:42:57 +0800

    feat: update routes

--- app/src/App.js
+++ app/src/App.js
@@ -25,17 +25,15 @@ function App() {
         <Route path="/search" element={<SearchDonor />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/main" element={<Main />}>
-          <Route index element={<Template />} />
-          <Route path="/main/myprofile" element={<Template />} />
-          <Route path="/main/search" element={<Template />} />
-          <Route path="/main/profile/:nameID" element={<Template />} />
-          <Route path="/main/requests" element={<Template />} />
-          <Route path="/main/active" element={<Template />} />
-          <Route path="/main/history/completed" element={<Template />} />
-          <Route path="/main/history/declined" element={<Template />} />
-          <Route path="/main/history/cancelled" element={<Template />} />
-        </Route>
+        <Route path="/main" element={<Main />} />
+        <Route path="/main/search" element={<Template />} />
+        <Route path="/main/myprofile" element={<Template />} />
+        <Route path="/main/profile/:nameID" element={<Template />} />
+        <Route path="/main/requests" element={<Template />} />
+        <Route path="/main/active" element={<Template />} />
+        <Route path="/main/history/completed" element={<Template />} />
+        <Route path="/main/history/declined" element={<Template />} />
+        <Route path="/main/history/cancelled" element={<Template />} />
       </Routes>
     </MainContext.Provider>
   );
