8fe70a3549361a90790a42022b34e961bd37fa18
MrYious <rosariomark37@gmail.com>
2022-11-17 10:18:52 +0800

    feat: add alerts to main page

--- app/src/pages/Main.js
+++ app/src/pages/Main.js
@@ -1,19 +1,53 @@
-import { useEffect, useState } from 'react';
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
 
+import { FaTimes } from 'react-icons/fa';
+import { MainContext } from '../App.js'
 import NavigationBar from '../components/NavigationBar';
+import axios  from "axios";
 
 const Main = () => {
+  const contextData = useContext(MainContext);
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const [alert, setAlert] = useState({
+    show: false,
+    header: '',
+    message: '',
+    isError: false
+  });
+
+  useEffect(() => {
+    if(location.state){
+      setAlert({
+        show: true,
+        header: location.state.message,
+        isError: location.state.isError,
+      });
+    }
+  }, [location])
 
   return (
     <div className='bg-blue-50'>
       <section className='flex flex-col w-full min-h-screen '>
+        {/* ALERT */}
+        {
+          alert.show &&
+          <div className={`flex items-start justify-between gap-5 w-[30%] border ${ alert.isError ? 'bg-red-100 border-red-400 text-red-700' : 'bg-green-100 border-green-400 text-green-700'} p-4 rounded fixed z-[2] bottom-0 m-5`} role="alert">
+            <div>
+              <div className="py-1 font-bold">
+                {alert.header}
+              </div>
+              {alert.message}
+            </div>
+            <FaTimes onClick={()=>{setAlert({...alert, show: false})}} className={`-mt-2 -mr-2 text-2xl  ${ alert.isError ? 'text-red-900' : 'text-green-900'}  cursor-pointer`}/>
+          </div>
+        }
         <NavigationBar />
         <div className='flex justify-center items-center flex-col min-h-[100vh] '>
           Main
         </div>
-      </section>
-      <section className='flex items-center justify-center w-full min-h-screen '>
-        Main
       </section>
     </div>
   );
