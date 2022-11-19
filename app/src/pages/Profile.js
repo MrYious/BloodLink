39cc4a95040d65f4a6806e3f52c67565d0c0aeef
MrYious <rosariomark37@gmail.com>
2022-11-19 09:25:00 +0800

    feat: add modal

--- app/src/pages/Profile.js
+++ app/src/pages/Profile.js
@@ -39,7 +39,7 @@ const Profile = () => {
   }, [location])
 
   const handleOpenReview = (i) => {
-    
+    setShowModal(true);
   }
 
   return (
@@ -58,7 +58,50 @@ const Profile = () => {
             <FaTimes onClick={()=>{setAlert({...alert, show: false})}} className={`-mt-2 -mr-2 text-2xl  ${ alert.isError ? 'text-red-900' : 'text-green-900'}  cursor-pointer`}/>
           </div>
         }
-        
+        {showModal && (
+        <>
+          <div
+            className="fixed inset-0 z-50 flex items-center justify-center overflow-x-hidden overflow-y-auto outline-none focus:outline-none"
+          >
+            <div className="relative w-auto max-w-3xl mx-auto my-6">
+              {/*content*/}
+              <div className="relative flex flex-col w-full bg-white border-0 rounded-lg shadow-lg outline-none focus:outline-none">
+                {/*header*/}
+                <div className="flex items-start justify-between p-5 border-b border-solid rounded-t border-slate-200">
+                  <h3 className="text-xl font-semibold">
+                    Modal Title
+                  </h3>
+                  <FaTimes onClick={() => setShowModal(false)} className='text-xl cursor-pointer ' />
+                </div>
+                {/*body*/}
+                <div className="flex flex-col p-6 text-sm">
+                  <p className="leading-relaxed text-slate-500">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae minima sed nemo assumenda veritatis, sit quaerat consectetur. At, dolores. Facilis quidem aperiam unde quod nesciunt ullam, amet adipisci est quae quo libero! Aspernatur ut nisi quidem vero culpa facere inventore vitae eligendi provident? Quasi, cupiditate, magnam, numquam labore tenetur quo iure deleniti amet veniam inventore consequatur accusamus veritatis nam adipisci? Provident, ex ducimus ab, consequuntur sit, nihil perspiciatis veritatis voluptates laborum asperiores dolorum. Placeat nam eum quidem quos sed perferendis dolor illum a! Reprehenderit perferendis, at facilis suscipit ipsam commodi porro similique eaque a doloremque earum. Eius atque ab fuga!
+                  </p>
+                </div>
+                {/*footer*/}
+                <div className="flex items-center justify-end p-6 border-t border-solid rounded-b border-slate-200">
+                  <button
+                    className="px-6 py-2 mb-1 mr-1 text-xs font-bold text-red-500 uppercase transition-all duration-150 ease-linear outline-none hover:underline background-transparent focus:outline-none"
+                    type="button"
+                    onClick={() => setShowModal(false)}
+                  >
+                    Close
+                  </button>
+                  <button
+                    className="px-6 py-3 mb-1 mr-1 text-xs font-bold text-white uppercase transition-all duration-150 ease-linear rounded shadow outline-none bg-emerald-500 hover:bg-emerald-700 active:bg-emerald-600 hover:shadow-lg focus:outline-none"
+                    type="button"
+                    onClick={() => setShowModal(false)}
+                  >
+                    Save Changes
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="fixed inset-0 z-40 bg-black opacity-25"></div>
+        </>
+        )}
         <MainNavigationBar />
         <div className='flex flex-col min-h-[100vh] '>
           <div className="h-[10vh]"></div>
@@ -309,7 +352,7 @@ const Profile = () => {
                       </div>
                     </div>
                     {/* 2 */}
-                    <div className='flex w-[48%] gap-2 p-2 border border-black rounded cursor-pointer shrink-0 hover:bg-gray-200'>
+                    <div onClick={()=>{handleOpenReview(1)}} className='flex w-[48%] gap-2 p-2 border border-black rounded cursor-pointer shrink-0 hover:bg-gray-200'>
                       <div className='flex items-center justify-center p-2 text-2xl'>
                         <FaQuoteLeft />
                       </div>
@@ -330,7 +373,7 @@ const Profile = () => {
                       </div>
                     </div>
                     {/* 3 */}
-                    <div className='flex w-[48%] gap-2 p-2 border border-black rounded cursor-pointer shrink-0 hover:bg-gray-200'>
+                    <div onClick={()=>{handleOpenReview(1)}} className='flex w-[48%] gap-2 p-2 border border-black rounded cursor-pointer shrink-0 hover:bg-gray-200'>
                       <div className='flex items-center justify-center p-2 text-2xl'>
                         <FaQuoteLeft />
                       </div>
--- app/src/pages/scratch.js
+++ app/src/pages/scratch.js
@@ -1,7 +1,7 @@
-import React from "react";
+import {useState} from "react";
 
 export default function Modal() {
-  const [showModal, setShowModal] = React.useState(false);
+  const [showModal, setShowModal] = useState(false);
   return (
     <>
       <button
@@ -11,7 +11,6 @@ export default function Modal() {
       >
         Open regular modal
       </button>
-      
       {showModal && (
         <>
           <div
@@ -22,39 +21,28 @@ export default function Modal() {
               <div className="relative flex flex-col w-full bg-white border-0 rounded-lg shadow-lg outline-none focus:outline-none">
                 {/*header*/}
                 <div className="flex items-start justify-between p-5 border-b border-solid rounded-t border-slate-200">
-                  <h3 className="text-3xl font-semibold">
+                  <h3 className="text-xl font-semibold">
                     Modal Title
                   </h3>
-                  <button
-                    className="float-right p-1 ml-auto text-3xl font-semibold leading-none text-black bg-transparent border-0 outline-none opacity-5 focus:outline-none"
-                    onClick={() => setShowModal(false)}
-                  >
-                    <span className="block w-6 h-6 text-2xl text-black bg-transparent outline-none opacity-5 focus:outline-none">
-                      ×
-                    </span>
-                  </button>
+                  <FaTimes onClick={() => setShowModal(false)} className='text-xl cursor-pointer ' />
                 </div>
                 {/*body*/}
-                <div className="relative flex-auto p-6">
-                  <p className="my-4 text-lg leading-relaxed text-slate-500">
-                    I always felt like I could do anything. That’s the main
-                    thing people are controlled by! Thoughts- their perception
-                    of themselves! They're slowed down by their perception of
-                    themselves. If you're taught you can’t do anything, you
-                    won’t do anything. I was taught I could do everything.
+                <div className="flex flex-col p-6 text-sm">
+                  <p className="leading-relaxed text-slate-500">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae minima sed nemo assumenda veritatis, sit quaerat consectetur. At, dolores. Facilis quidem aperiam unde quod nesciunt ullam, amet adipisci est quae quo libero! Aspernatur ut nisi quidem vero culpa facere inventore vitae eligendi provident? Quasi, cupiditate, magnam, numquam labore tenetur quo iure deleniti amet veniam inventore consequatur accusamus veritatis nam adipisci? Provident, ex ducimus ab, consequuntur sit, nihil perspiciatis veritatis voluptates laborum asperiores dolorum. Placeat nam eum quidem quos sed perferendis dolor illum a! Reprehenderit perferendis, at facilis suscipit ipsam commodi porro similique eaque a doloremque earum. Eius atque ab fuga!
                   </p>
                 </div>
                 {/*footer*/}
                 <div className="flex items-center justify-end p-6 border-t border-solid rounded-b border-slate-200">
                   <button
-                    className="px-6 py-2 mb-1 mr-1 text-sm font-bold text-red-500 uppercase transition-all duration-150 ease-linear outline-none background-transparent focus:outline-none"
+                    className="px-6 py-2 mb-1 mr-1 text-xs font-bold text-red-500 uppercase transition-all duration-150 ease-linear outline-none hover:underline background-transparent focus:outline-none"
                     type="button"
                     onClick={() => setShowModal(false)}
                   >
                     Close
                   </button>
                   <button
-                    className="px-6 py-3 mb-1 mr-1 text-sm font-bold text-white uppercase transition-all duration-150 ease-linear rounded shadow outline-none bg-emerald-500 active:bg-emerald-600 hover:shadow-lg focus:outline-none"
+                    className="px-6 py-3 mb-1 mr-1 text-xs font-bold text-white uppercase transition-all duration-150 ease-linear rounded shadow outline-none bg-emerald-500 hover:bg-emerald-700 active:bg-emerald-600 hover:shadow-lg focus:outline-none"
                     type="button"
                     onClick={() => setShowModal(false)}
                   >
