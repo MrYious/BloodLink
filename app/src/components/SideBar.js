12e462ed9903cea5f6294c929f8963dac9dc2460
MrYious <rosariomark37@gmail.com>
2022-11-20 22:16:04 +0800

    style: update layout and icons

--- app/src/components/SideBar.js
+++ app/src/components/SideBar.js
@@ -1,4 +1,4 @@
-import { FaAngleDoubleRight, FaBars, FaTimes } from 'react-icons/fa';
+import { FaAngleDoubleRight, FaClock, FaInbox, FaPlus, FaWindowClose } from 'react-icons/fa';
 
 import { Link } from "react-router-dom";
 import profilepic from '../assets/images/profilepic.jpg'
@@ -14,12 +14,12 @@ const SideBar = () => {
     return (
         <>
             {/* 0 */}
-            <div className="flex flex-col w-[70px] shrink-0"></div>
+            <div className="flex flex-col w-[50px] shrink-0"></div>
             {/* 1 */}
-            <div onMouseEnter={() => {setIsOpen(true)}} onClick={() => {setIsOpen(true)}} onMouseLeave={() => {setIsOpen(false)}}  className={`flex flex-col p-2 ${isOpen ? 'w-[300px]' : 'w-[70px] items-center'}  bg-gray-100 z-[1] h-full shrink-0 none fixed border-r-[1px] border-black`}>
-                <Link to={'/main/profile'} className="flex items-center gap-2 mb-3 rounded cursor-pointer h-14 hover:bg-gray-300">
+            <div onMouseEnter={() => {setIsOpen(true)}} onClick={() => {setIsOpen(true)}} onMouseLeave={() => {setIsOpen(false)}}  className={`flex flex-col p-2 ${isOpen ? 'w-[300px]' : 'w-[50px] items-center'}  bg-gray-50 drop-shadow-xl border-r-[1px] z-[1] h-full shrink-0 fixed border-gray-400`}>
+                <Link to={'/main/profile'} className="flex items-center h-12 gap-2 mb-3 rounded cursor-pointer hover:bg-gray-300">
                     <div className='flex items-center justify-center h-full w-14 shrink-0'>
-                        <img className='w-full border border-black rounded-full shadow cursor-pointer shadow-black'  src={ profile !== 'null' ? profile : profilepic} alt="profilepic" />
+                        <img className='w-10 border border-black rounded-full shadow cursor-pointer shadow-black'  src={ profile !== 'null' ? profile : profilepic} alt="profilepic" />
                     </div>
                     {
                         isOpen &&
@@ -30,7 +30,7 @@ const SideBar = () => {
                 </Link>
                 <Link to={'/main/requests'} className="flex items-center h-12 gap-2 rounded cursor-pointer hover:bg-gray-300">
                     <div className='flex items-center justify-center h-full w-14 shrink-0'>
-                        <FaAngleDoubleRight className='text-2xl'/>
+                        <FaPlus className='text-xl'/>
                     </div>
                     {
                         isOpen &&
@@ -41,7 +41,7 @@ const SideBar = () => {
                 </Link>
                 <Link to={'/main/active'} className="flex items-center h-12 gap-2 rounded cursor-pointer hover:bg-gray-300">
                     <div className='flex items-center justify-center h-full w-14 shrink-0'>
-                        <FaAngleDoubleRight className='text-2xl'/>
+                        <FaInbox className='text-xl'/>
                     </div>
                     {
                         isOpen &&
@@ -52,7 +52,7 @@ const SideBar = () => {
                 </Link>
                 <Link to={'/main/history/completed'} className="flex items-center h-12 gap-2 rounded cursor-pointer hover:bg-gray-300">
                     <div className='flex items-center justify-center h-full w-14 shrink-0'>
-                        <FaAngleDoubleRight className='text-2xl'/>
+                        <FaClock className='text-xl'/>
                     </div>
                     {
                         isOpen &&
@@ -63,7 +63,7 @@ const SideBar = () => {
                 </Link>
                 <Link to={'/main/history/cancelled'} className="flex items-center h-12 gap-2 rounded cursor-pointer hover:bg-gray-300">
                     <div className='flex items-center justify-center h-full w-14 shrink-0'>
-                        <FaAngleDoubleRight className='text-2xl'/>
+                        <FaWindowClose className='text-xl'/>
                     </div>
                     {
                         isOpen &&
@@ -74,7 +74,7 @@ const SideBar = () => {
                 </Link>
                 <Link to={'/main/history/declined'} className="flex items-center h-12 gap-2 rounded cursor-pointer hover:bg-gray-300">
                     <div className='flex items-center justify-center h-full w-14 shrink-0'>
-                        <FaAngleDoubleRight className='text-2xl'/>
+                        <FaWindowClose className='text-xl'/>
                     </div>
                     {
                         isOpen &&
