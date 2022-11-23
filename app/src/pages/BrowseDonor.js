bd6b6be5d6b35ba3e2fc3d982f2f3c4bef17f8bc
MrYious <rosariomark37@gmail.com>
2022-11-23 22:55:41 +0800

    feat: update initial layouts

--- app/src/pages/BrowseDonor.js
+++ app/src/pages/BrowseDonor.js
@@ -4,6 +4,7 @@ import { useContext, useEffect, useState } from "react";
 import { FaTimes } from 'react-icons/fa';
 import { MainContext } from '../App.js'
 import MainNavigationBar from '../components/MainNavigationBar';
+import SideBar from '../components/SideBar.js';
 import axios  from "axios";
 
 const BrowseDonor = () => {
@@ -12,6 +13,7 @@ const BrowseDonor = () => {
   const navigate = useNavigate();
   const userId = localStorage.getItem('userID');
 
+  const [showModal, setShowModal] = useState(false);
   const [alert, setAlert] = useState({
     show: false,
     header: '',
@@ -36,29 +38,83 @@ const BrowseDonor = () => {
   }, [location])
 
   return (
-    <div className='bg-blue-50'>
-      <section className='flex flex-col w-full min-h-screen '>
-        {/* ALERT */}
-        {
-          alert.show &&
-          <div className={`flex items-start justify-between gap-5 w-[90%] md:w-[40%] lg:w-[30%]  border ${ alert.isError ? 'bg-red-100 border-red-400 text-red-700' : 'bg-green-100 border-green-400 text-green-700'} p-4 rounded fixed z-[2] bottom-0 m-5`} role="alert">
-            <div>
-              <div className="py-1 font-bold">
-                {alert.header}
-              </div>
-              {alert.message}
+    <section className='flex flex-col w-full min-h-screen '>
+      {/* ALERT */}
+      {
+        alert.show &&
+        <div className={`flex items-start justify-between gap-5 w-[90%] md:w-[40%] lg:w-[30%]  border ${ alert.isError ? 'bg-red-100 border-red-400 text-red-700' : 'bg-green-100 border-green-400 text-green-700'} p-4 rounded fixed z-[2] bottom-0 m-5`} role="alert">
+          <div>
+            <div className="py-1 font-bold">
+              {alert.header}
             </div>
-            <FaTimes onClick={()=>{setAlert({...alert, show: false})}} className={`-mt-2 -mr-2 text-2xl  ${ alert.isError ? 'text-red-900' : 'text-green-900'}  cursor-pointer`}/>
+            {alert.message}
           </div>
-        }
-        <MainNavigationBar />
-        <div className='flex justify-center items-center flex-col min-h-[100vh] '>
-          Main
+          <FaTimes onClick={()=>{setAlert({...alert, show: false})}} className={`-mt-2 -mr-2 text-2xl  ${ alert.isError ? 'text-red-900' : 'text-green-900'}  cursor-pointer`}/>
         </div>
-      </section><div className='flex justify-center items-center flex-col min-h-[90vh] '>
-          Main
+      }
+      {/* MODAL */}
+      {showModal && (
+      <>
+        <div
+          className="fixed inset-0 z-50 flex items-center justify-center overflow-x-hidden overflow-y-auto outline-none focus:outline-none"
+        >
+          <div className="relative w-auto max-w-3xl mx-auto my-6">
+            {/*content*/}
+            <div className="relative flex flex-col w-full bg-white border-0 rounded-lg shadow-lg outline-none focus:outline-none">
+              {/*header*/}
+              <div className="flex items-start justify-between p-5 border-b border-solid rounded-t border-slate-200">
+                <h3 className="text-xl font-semibold">
+                  Donor Review
+                </h3>
+                <FaTimes onClick={() => setShowModal(false)} className='text-xl cursor-pointer ' />
+              </div>
+              {/*body*/}
+              <div className="flex flex-col p-6 text-sm md:flex-row ">
+                {/* HERE */}
+              </div>
+              {/*footer*/}
+              <div className="flex items-center justify-end p-6 border-t border-solid rounded-b border-slate-200">
+                <button
+                  className="px-6 py-2 mb-1 mr-1 text-xs font-bold text-red-500 uppercase transition-all duration-150 ease-linear outline-none hover:underline background-transparent focus:outline-none"
+                  type="button"
+                  onClick={() => setShowModal(false)}
+                >
+                  Close
+                </button>
+                <button
+                  className="px-6 py-3 mb-1 mr-1 text-xs font-bold text-white uppercase transition-all duration-150 ease-linear rounded shadow outline-none bg-emerald-500 hover:bg-emerald-700 active:bg-emerald-600 hover:shadow-lg focus:outline-none"
+                  type="button"
+                  onClick={() => setShowModal(false)}
+                >
+                  Save Changes
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
-    </div>
+        <div className="fixed inset-0 z-40 bg-black opacity-25"></div>
+      </>
+      )}
+      <MainNavigationBar />
+      <div className='flex flex-col min-h-[100vh] '>
+        <div className="h-[10vh]"></div>
+        <div className="flex min-h-[90vh]">
+          {/* 1 */}
+          <SideBar />
+          {/* 2 */}
+          <div className="flex flex-col items-start w-full gap-5 p-5 bg-gray-100 lg:justify-around lg:flex-row">
+            {/* 1 */}
+            <div className="bg-gray-50 w-[100%] lg:w-[60%] flex items-center flex-col p-5 rounded drop-shadow-lg">
+              Main Contents
+            </div>
+            {/* 2 */}
+            <div className="bg-gray-50 w-[100%] lg:w-[33%] flex flex-col items-center p-5 rounded drop-shadow-lg">
+              Side Contents
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
 
--- app/src/pages/HistoryCancelled.js
+++ app/src/pages/HistoryCancelled.js
@@ -38,85 +38,83 @@ const HistoryCancelled = () => {
   }, [location])
 
   return (
-    <div className='bg-blue-50'>
-      <section className='flex flex-col w-full min-h-screen '>
-        {/* ALERT */}
-        {
-          alert.show &&
-          <div className={`flex items-start justify-between gap-5 w-[90%] md:w-[40%] lg:w-[30%]  border ${ alert.isError ? 'bg-red-100 border-red-400 text-red-700' : 'bg-green-100 border-green-400 text-green-700'} p-4 rounded fixed z-[2] bottom-0 m-5`} role="alert">
-            <div>
-              <div className="py-1 font-bold">
-                {alert.header}
-              </div>
-              {alert.message}
+    <section className='flex flex-col w-full min-h-screen '>
+      {/* ALERT */}
+      {
+        alert.show &&
+        <div className={`flex items-start justify-between gap-5 w-[90%] md:w-[40%] lg:w-[30%]  border ${ alert.isError ? 'bg-red-100 border-red-400 text-red-700' : 'bg-green-100 border-green-400 text-green-700'} p-4 rounded fixed z-[2] bottom-0 m-5`} role="alert">
+          <div>
+            <div className="py-1 font-bold">
+              {alert.header}
             </div>
-            <FaTimes onClick={()=>{setAlert({...alert, show: false})}} className={`-mt-2 -mr-2 text-2xl  ${ alert.isError ? 'text-red-900' : 'text-green-900'}  cursor-pointer`}/>
+            {alert.message}
           </div>
-        }
-        {/* MODAL */}
-        {showModal && (
-        <>
-          <div
-            className="fixed inset-0 z-50 flex items-center justify-center overflow-x-hidden overflow-y-auto outline-none focus:outline-none"
-          >
-            <div className="relative w-auto max-w-3xl mx-auto my-6">
-              {/*content*/}
-              <div className="relative flex flex-col w-full bg-white border-0 rounded-lg shadow-lg outline-none focus:outline-none">
-                {/*header*/}
-                <div className="flex items-start justify-between p-5 border-b border-solid rounded-t border-slate-200">
-                  <h3 className="text-xl font-semibold">
-                    Donor Review
-                  </h3>
-                  <FaTimes onClick={() => setShowModal(false)} className='text-xl cursor-pointer ' />
-                </div>
-                {/*body*/}
-                <div className="flex flex-col p-6 text-sm md:flex-row ">
-                  {/* HERE */}
-                </div>
-                {/*footer*/}
-                <div className="flex items-center justify-end p-6 border-t border-solid rounded-b border-slate-200">
-                  <button
-                    className="px-6 py-2 mb-1 mr-1 text-xs font-bold text-red-500 uppercase transition-all duration-150 ease-linear outline-none hover:underline background-transparent focus:outline-none"
-                    type="button"
-                    onClick={() => setShowModal(false)}
-                  >
-                    Close
-                  </button>
-                  <button
-                    className="px-6 py-3 mb-1 mr-1 text-xs font-bold text-white uppercase transition-all duration-150 ease-linear rounded shadow outline-none bg-emerald-500 hover:bg-emerald-700 active:bg-emerald-600 hover:shadow-lg focus:outline-none"
-                    type="button"
-                    onClick={() => setShowModal(false)}
-                  >
-                    Save Changes
-                  </button>
-                </div>
+          <FaTimes onClick={()=>{setAlert({...alert, show: false})}} className={`-mt-2 -mr-2 text-2xl  ${ alert.isError ? 'text-red-900' : 'text-green-900'}  cursor-pointer`}/>
+        </div>
+      }
+      {/* MODAL */}
+      {showModal && (
+      <>
+        <div
+          className="fixed inset-0 z-50 flex items-center justify-center overflow-x-hidden overflow-y-auto outline-none focus:outline-none"
+        >
+          <div className="relative w-auto max-w-3xl mx-auto my-6">
+            {/*content*/}
+            <div className="relative flex flex-col w-full bg-white border-0 rounded-lg shadow-lg outline-none focus:outline-none">
+              {/*header*/}
+              <div className="flex items-start justify-between p-5 border-b border-solid rounded-t border-slate-200">
+                <h3 className="text-xl font-semibold">
+                  Donor Review
+                </h3>
+                <FaTimes onClick={() => setShowModal(false)} className='text-xl cursor-pointer ' />
               </div>
-            </div>
-          </div>
-          <div className="fixed inset-0 z-40 bg-black opacity-25"></div>
-        </>
-        )}
-        <MainNavigationBar />
-        <div className='flex flex-col min-h-[100vh] '>
-          <div className="h-[10vh]"></div>
-          <div className="flex min-h-[90vh]">
-            {/* 1 */}
-            <SideBar />
-            {/* 2 */}
-            <div className="flex flex-col items-start w-full gap-5 p-5 bg-gray-100 lg:justify-around lg:flex-row">
-              {/* 1 */}
-              <div className="bg-gray-50 w-[100%] lg:w-[60%] flex items-center flex-col p-5 rounded drop-shadow-lg">
-                Main Contents
+              {/*body*/}
+              <div className="flex flex-col p-6 text-sm md:flex-row ">
+                {/* HERE */}
               </div>
-              {/* 2 */}
-              <div className="bg-gray-50 w-[100%] lg:w-[33%] flex flex-col items-center p-5 rounded drop-shadow-lg">
-                Side Contents
+              {/*footer*/}
+              <div className="flex items-center justify-end p-6 border-t border-solid rounded-b border-slate-200">
+                <button
+                  className="px-6 py-2 mb-1 mr-1 text-xs font-bold text-red-500 uppercase transition-all duration-150 ease-linear outline-none hover:underline background-transparent focus:outline-none"
+                  type="button"
+                  onClick={() => setShowModal(false)}
+                >
+                  Close
+                </button>
+                <button
+                  className="px-6 py-3 mb-1 mr-1 text-xs font-bold text-white uppercase transition-all duration-150 ease-linear rounded shadow outline-none bg-emerald-500 hover:bg-emerald-700 active:bg-emerald-600 hover:shadow-lg focus:outline-none"
+                  type="button"
+                  onClick={() => setShowModal(false)}
+                >
+                  Save Changes
+                </button>
               </div>
             </div>
           </div>
         </div>
-      </section>
-    </div>
+        <div className="fixed inset-0 z-40 bg-black opacity-25"></div>
+      </>
+      )}
+      <MainNavigationBar />
+      <div className='flex flex-col min-h-[100vh] '>
+        <div className="h-[10vh]"></div>
+        <div className="flex min-h-[90vh]">
+          {/* 1 */}
+          <SideBar />
+          {/* 2 */}
+          <div className="flex flex-col items-start w-full gap-5 p-5 bg-gray-100 lg:justify-around lg:flex-row">
+            {/* 1 */}
+            <div className="bg-gray-50 w-[100%] lg:w-[60%] flex items-center flex-col p-5 rounded drop-shadow-lg">
+              Main Contents
+            </div>
+            {/* 2 */}
+            <div className="bg-gray-50 w-[100%] lg:w-[33%] flex flex-col items-center p-5 rounded drop-shadow-lg">
+              Side Contents
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
 
