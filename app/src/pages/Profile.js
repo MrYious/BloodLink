d85b8f835e0cd6d5f808e776fce0a0dc46ddc398
MrYious <rosariomark37@gmail.com>
2022-12-12 23:00:37 +0800

    feat: added form for reporting user

--- app/src/pages/Profile.js
+++ app/src/pages/Profile.js
@@ -1,4 +1,4 @@
-import { FaAngleLeft, FaAngleRight, FaFacebookSquare, FaInstagramSquare, FaQuoteLeft, FaRegStar, FaStar, FaTimes, FaTwitterSquare } from 'react-icons/fa';
+import { FaAngleLeft, FaAngleRight, FaExclamation, FaFacebookSquare, FaInstagramSquare, FaQuoteLeft, FaRegStar, FaStar, FaTimes, FaTwitterSquare } from 'react-icons/fa';
 import { Link, useLocation, useNavigate, useParams } from "react-router-dom";
 import { useContext, useEffect, useState } from "react";
 
@@ -129,8 +129,10 @@ const Profile = () => {
   })
 
   const [requestMessage, setRequestMessage] = useState('');
+  const [reason, setReason] = useState('');
 
   const [showReviewModal, setShowReviewModal] = useState(false);
+  const [showReportModal, setShowReportModal] = useState(false);
   const [showRequestModal, setShowRequestModal] = useState(false);
   const [alert, setAlert] = useState({
     show: false,
@@ -162,8 +164,9 @@ const Profile = () => {
   }, [location])
 
   useEffect(() => {
-    setRequestMessage('')
-  }, [showRequestModal])
+    setRequestMessage('');
+    setReason('');
+  }, [showRequestModal, showReportModal])
 
   const fetchUserProfile = () => {
     const data = {
@@ -196,6 +199,10 @@ const Profile = () => {
 
   const handleOpenRequestDonation = () => {
     setShowRequestModal(true);
+  }
+
+  const handleOpenReportUser = () => {
+    setShowReportModal(true);
   }
 
   const handleRequestDonation = () => {
@@ -235,12 +242,24 @@ const Profile = () => {
     }
   }
 
+  const handleReportUser = () => {
+    if(reason === ''){
+      setAlert({
+        show: true,
+        header: 'Please state the reason for reporting the user.',
+        isError: true,
+      });
+    } else {
+      setShowReportModal(false);
+    }
+  }
+
   return (
     <section className='flex flex-col w-full min-h-screen '>
       {/* ALERT */}
       {
         alert.show &&
-        <div className={`flex items-start justify-between gap-5 w-[90%] md:w-[40%] lg:w-[30%]  border ${ alert.isError ? 'bg-red-100 border-red-400 text-red-700' : 'bg-green-100 border-green-400 text-green-700'} p-4 rounded fixed z-[2] bottom-0 m-5`} role="alert">
+        <div className={`flex z-[100] items-start justify-between gap-5 w-[90%] md:w-[40%] lg:w-[30%]  border ${ alert.isError ? 'bg-red-100 border-red-400 text-red-700' : 'bg-green-100 border-green-400 text-green-700'} p-4 rounded fixed z-[2] bottom-0 m-5`} role="alert">
           <div>
             <div className="py-1 font-bold">
               {alert.header}
@@ -368,6 +387,53 @@ const Profile = () => {
         <div className="fixed inset-0 z-40 bg-black opacity-25"></div>
       </>
       )}
+      {/* REPORT MODAL */}
+      {showReportModal && (
+      <>
+        <div
+          className="fixed inset-0 z-50 flex items-center justify-center overflow-x-hidden overflow-y-auto outline-none focus:outline-none"
+        >
+          <div className="relative w-auto max-w-3xl mx-auto my-6">
+            {/*content*/}
+            <div className="relative flex flex-col w-full bg-white border-0 rounded-lg shadow-lg outline-none focus:outline-none">
+              {/*header*/}
+              <div className="flex items-start justify-between p-5 border-b border-solid rounded-t border-slate-200">
+                <h3 className="text-xl font-semibold">
+                  Report User
+                </h3>
+                <FaTimes onClick={() => setShowReportModal(false)} className='text-xl cursor-pointer ' />
+              </div>
+              {/*body*/}
+              <div className="flex flex-col gap-2 p-6 text-sm">
+                <div>Do you want to report <b>{userProfile.user.firstname + ' ' + userProfile.user.lastname}</b>?</div>
+                <div className='flex flex-col gap-2 pt-5 '>
+                  <div className='text-xs italic'>Please provide the reason on why this user should be banned (required)</div>
+                  <textarea value={reason} onChange={(e)=>{setReason(e.target.value)}} placeholder="Reason" rows={5} maxLength={200} className='w-full p-1 border border-gray-700 rounded outline-none resize-none '></textarea>
+                </div>
+              </div>
+              {/*footer*/}
+              <div className="flex items-center justify-end p-6 border-t border-solid rounded-b border-slate-200">
+                <button
+                  className="px-6 py-2 mb-1 mr-1 text-xs font-bold text-red-500 uppercase transition-all duration-150 ease-linear outline-none hover:underline background-transparent focus:outline-none"
+                  type="button"
+                  onClick={() => setShowReportModal(false)}
+                >
+                  Cancel
+                </button>
+                <button
+                  className="px-6 py-3 mb-1 mr-1 text-xs font-bold text-white uppercase transition-all duration-150 ease-linear rounded shadow outline-none bg-emerald-500 hover:bg-emerald-700 active:bg-emerald-600 hover:shadow-lg focus:outline-none"
+                  type="button"
+                  onClick={handleReportUser}
+                >
+                  Submit Report
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="fixed inset-0 z-40 bg-black opacity-25"></div>
+      </>
+      )}
       <MainNavigationBar />
       <div className='flex flex-col min-h-[100vh] '>
         <div className="h-[10vh]"></div>
@@ -422,8 +488,9 @@ const Profile = () => {
                         <Link to={'/main/update'} className='py-1 text-xs bg-red-300 border border-red-500 rounded-full px-7 hover:bg-red-500'>Edit Profile</Link>
                       </div>
                     :
-                      <div>
+                      <div className='flex gap-2'>
                         <button onClick={handleOpenRequestDonation} className='py-1 text-xs bg-blue-300 border border-blue-500 rounded-full px-7 hover:bg-blue-500'>Request Blood Donation</button>
+                        <button onClick={handleOpenReportUser} className='px-5 py-1 text-xs bg-red-300 border border-red-500 rounded-full hover:bg-red-500'> Report </button>
                       </div>
                   }
                 </div>
